58fd5716d523e3553b9b3460578e7bbae8714141
94mark <90877724+94mark@users.noreply.github.com>
2022-01-15 22:02:21 +0900

    VideoFrame.cs

---
+++ VR_360Player/Assets/Scripts/VideoFrame.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video; //VideoPlayer 기능을 사용하기 위한 네임스페이스
+
+public class VideoFrame : MonoBehaviour
+{
+    //Video Player 컴포넌트
+    VideoPlayer vp;
+    
+    void Start()
+    {
+        //현재 오브젝트의 비디오 플레이어 컴포넌트 정보를 가지고 온다
+        vp = GetComponent<VideoPlayer>();
+        //자동 재생되는 것을 막는다
+        vp.Stop();
+    }
+
+    void Update()
+    {
+        //S를 누르면 정지
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            vp.Stop();
+        }
+        //스페이스 바를 눌렀을 때 재생 또는 일시 정지
+        if(Input.GetKeyDown("space"))
+        {
+            //현재 비디오 플레이어가 플레이 상태인지 확인
+            if(vp.isPlaying)
+            {
+                //재생 중이라면 일시 정지
+                vp.Pause();
+            }
+            else
+            {
+                //그렇지 않다면 플레이
+                vp.Play();
+            }
+        }
+    }
+}
