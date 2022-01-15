db1955ce54d2b8e73536ef8d2ff329c4b43220c0
94mark <90877724+94mark@users.noreply.github.com>
2022-01-15 22:48:18 +0900

    Update Video360Play.cs

--- VR_360Player/Assets/Scripts/Video360Play.cs
+++ VR_360Player/Assets/Scripts/Video360Play.cs
@@ -24,11 +24,36 @@ public class Video360Play : MonoBehaviour
         //컴퓨터에서 영상을 변경하기 위한 기능
         if(Input.GetKeyDown(KeyCode.LeftBracket)) //왼쪽 대괄호 입력 시 이전 영상
         {
-            vp.clip = vcList[0];
+            SwapVideoClip(false);
         }
         else if(Input.GetKeyDown(KeyCode.RightBracket))  //오른쪽 대괄호 입력 시 이전 영상
         {
-            vp.clip = vcList[1];
+            SwapVideoClip(true);
         }
+    }
+    //인터랙션을 위해 함수를 퍼블릭으로 서언
+    //배열의 인덱스 번호를 기준으로 영상을 교체, 재생하기 위한 함수
+    //인자 값인 isNext가 true이면 다음 영상, false면 이전 영상 재생
+    public void SwapVideoClip(bool isNext)
+    {
+        //현재 재생 중인 영상의 넘버를 기준으로 체크
+        //이전 영상 번호는 현재 영상보다 배열에서 인덱스 번호가 1 작다
+        //다음 영상 번호는 현재 영상보다 배열에서 인덱스 번호가 1 크다
+        int setVCnum = curVCidx; //현재 재생 중인 영상의 넘버를 입력한다
+        vp.Stop(); //현재 재생 중인 비디오 클립을 중지한다
+        //재생될 영상을 고르기 위한 과정
+        if(isNext) //isNext 변수가 참이라면 리스트의 다음 영상 재생
+        {
+            //리스트 전체 길이보다 크면 클립을 리스트의 첫 번째 영상으로 지정
+            setVCnum = ((setVCnum + 1) % vcList.Length);
+        }
+        else
+        {
+            //배열의 이전 영상을 재생
+            setVCnum = ((setVCnum - 1) + vcList.Length) % vcList.Length;
+        }
+        vp.clip = vcList[setVCnum]; //클립을 변경
+        vp.Play(); //바뀐 클립을 재생
+        curVCidx = setVCnum; //바뀐 클립의 영상 번호를 업데이트
     }
 }
