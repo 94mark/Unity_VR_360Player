fe849fdbfe33e661e08f1c56140bb9e28b21e0d5
94mark <90877724+94mark@users.noreply.github.com>
2022-01-16 19:44:31 +0900

    Update GazePointerCtrl

--- VR_360Player/Assets/Scripts/GazePointerCtrl.cs
+++ VR_360Player/Assets/Scripts/GazePointerCtrl.cs
@@ -12,9 +12,17 @@ public class GazePointerCtrl : MonoBehaviour
 
     Vector3 defaultScale; //UI 기본 스케일을 저장해두기 위한 값
     public float uiScaleVal = 1f; //UI카메라가 1m 일 때 배율
+
+    bool isHitObj; //인터렉션이 일어나는 오브젝트에 시선이 닿으면 true, 닿지 않으면 false
+    GameObject prevHitObj; //이전 프레임의 시선이 머물렀던 오브젝트 정보를 담기 위한 변수
+    GameObject curHitObj; //현재 프레임의 시선이 머물렀던 오브젝트 정보를 담기 위한 변수
+    float curGazeTime = 0; //시선이 머무르는 시간을 저장하기 위한 변수
+    public float gazeChargeTime = 3f; //시선이 머문 시간을 체크하기 위한 기준 시간 3초(필요에 따라 수정)
+
     void Start()
     {
         defaultScale = uiCanvas.localScale; //오브젝트가 갖는 기본 스케일 값
+        curGazeTime = 0; //시선을 유지하는지 체크하기 위한 변수 초기화
     }
 
     void Update()
@@ -30,6 +38,11 @@ public class GazePointerCtrl : MonoBehaviour
         {
             uiCanvas.localScale = defaultScale * uiScaleVal * hitInfo.distance;
             uiCanvas.position = transform.forward * hitInfo.distance;
+            if(hitInfo.transform.tag == "GazeObj")
+            {
+                isHitObj = true;
+            }
+            curHitObj = hitInfo.transform.gameObject;
         }
         else //4.아무것도 부딪히지 않으면 기본 스케일 값으로 uiCanvas의 크기를 조절한다
         {
@@ -38,5 +51,32 @@ public class GazePointerCtrl : MonoBehaviour
         }
         //5.uiCanvas가 항상 카메라 오브젝트를 바라보게 한다
         uiCanvas.forward = transform.forward * -1;
+
+        //GazeObj에 레이가 닿았을 때 실행
+        if(isHitObj)
+        {
+            if(curHitObj == prevHitObj) //현재 프레임과 이전 프레임의 오브젝트가 같아야 시간 증가
+            {
+                //인터렉션이 발생해야 하는 오브젝트에 시선이 고정돼 있다면 시간 증가
+                curGazeTime += Time.deltaTime;
+            }
+            else
+            {
+                //이전 프레임의 영상 정보를 업데이트한다
+                prevHitObj = curHitObj;
+            }
+        }
+        else //시선이 벗어났거나 GazeObj가 아니라면 시간을 초기화
+        {
+            curGazeTime = 0;
+            prevHitObj = null; //prevHitObj 정보를 지운다
+        }
+        //시선이 머문 시간을 0과 촤댓값 사이로 한다
+        curGazeTime = Mathf.Clamp(curGazeTime, 0, gazeChargeTime);
+        //ui image의 fillAmount를 업데이트한다
+        gazeImg.fillAmount = curGazeTime / gazeChargeTime;
+
+        isHitObj = false; //모든 처리를 끝내면 isHitObj를 false로 한다
+        curHitObj = null; //curHitObj 정보를 지운다
     }
 }
